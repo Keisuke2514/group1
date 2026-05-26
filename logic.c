1e2844285da23a6900ea7b14ed4a0e99d9330a87
Keisuke2514 <s2514@oita.kosen-ac.jp>
2026-05-26 11:45:05 +0900

    add-createproblem

--- logic.c
+++ logic.c
@@ -1,1 +1,11 @@
-test
+#include "logic.h"
+#include <stdio.h>
+
+void createproblem(){
+    problem[id].id=id+1;
+    printf("問題を入力:");
+    scanf("%s",&problem[id].question);
+    printf("答えを入力:");
+    scanf("%s",&problem[id].answer);
+    id++;
+}
--- logic.h
+++ logic.h
@@ -1,7 +1,9 @@
 #ifndef LOGIC_H
 #define LOGIC_H
+int id=0;
+Problem problem[1024];
  typedef struct{
     int id;
     char question[1024];
     char answer[1024];
- }problem;
+ }Problem;
