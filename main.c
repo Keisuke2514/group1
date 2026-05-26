21a595d0123401374e382d5a0df9e380dbf4ed65
toku-osaru <s2528@oita.kosen-ac.jp>
2026-05-26 11:20:09 +0900

    menu

--- main.c
+++ main.c
@@ -1,1 +1,33 @@
-test
+#include<stdio.h>
+
+int main(void){
+    int que = 0;
+    int cor = 0;
+    int menu;
+
+
+    while(1){
+        printf("＝メニュー＝\n1:作問    2:練習    3:テスト  4:問題一覧\n");
+        scanf("%d",&menu);
+
+        if(menu == 1){
+            //kannsuu
+        }
+
+        else if(menu == 2){
+            printf("");
+        }
+
+        else if(menu == 3){
+            printf("");
+        }
+
+        else if(menu == 4){
+            printf("");
+        }
+
+        else{
+            printf("1～3の数字を入力してください");
+        }
+    }
+}
