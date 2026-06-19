e2486dc2bf897424e044665d807d1c6d5c37cb1d
toku-osaru <s2528@oita.kosen-ac.jp>
2026-06-19 15:00:28 +0900

    add-while

--- main.c
+++ main.c
@@ -3,27 +3,33 @@
 
 int main(void){
     id = lord_cards("date.csv",problem,1024);
-    int menu;
 
-    menu = ui();
+    while(1){
+        int menu;
 
-    switch(menu)
-    {
-        case 0:
-            printf("問題作成\n");
-            break;
+        menu = ui();
 
-        case 1:
-            printf("練習\n");
-            break;
+        switch(menu)
+        {
+            case 0:
+                printf("問題作成\n");
+                break;
 
-        case 2:
-            printf("テスト\n");
-            break;
+            case 1:
+                printf("練習\n");
+                break;
 
-        case 3:
-            printf("問題一覧\n");
-            break;
-    }
-    
+            case 2:
+                printf("テスト\n");
+                break;
+
+            case 3:
+                printf("問題一覧\n");
+                break;
+
+            case 4:
+                printf("プログラムを終了します");
+                return 0;
+        }
+    }   
 }
--- ui.c
+++ ui.c
@@ -34,27 +34,37 @@ int ui(void)
             printf("\t\x1b[33m[問題一覧]\x1b[39m\n");
         else
             printf("\t[問題一覧]\n");
+        if(pos == 4)
+            printf("\n\x1b[33m[終了]\x1b[39m\n");
+        else
+            printf("\n[終了]\n");
 
         key = getch();
 
         if(key == 'w' || key == 'W')
         {
-            if(pos >= 2) pos -= 2;
+            if(pos == 2) pos = 0;
+            else if(pos == 3) pos = 1;
+            else if(pos == 4) pos = 2;   // 4から上
         }
 
         if(key == 's' || key == 'S')
         {
-            if(pos <= 1) pos += 2;
+            if(pos == 0) pos = 2;
+            else if(pos == 1) pos = 3;
+            else if(pos == 2 || pos == 3) pos = 4;  // 一番下へ
         }
 
         if(key == 'a' || key == 'A')
         {
-            if(pos % 2 == 1) pos--;
+            if(pos == 1) pos = 0;
+            else if(pos == 3) pos = 2;
         }
 
         if(key == 'd' || key == 'D')
         {
-            if(pos % 2 == 0) pos++;
+            if(pos == 0) pos = 1;
+            else if(pos == 2) pos = 3;
         }
 
         if(key == 13)
