070b8339f27208446d8b9862a40eaa0444cfd4c9
Keisuke2514 <s2514@oita.kosen-ac.jp>
2026-06-19 16:09:50 +0900

    proto

--- logic.c
+++ logic.c
@@ -1,5 +1,7 @@
 #include "logic.h"
 #include <stdio.h>
+#include<string.h>
+#include<time.h>
 
 void createproblem(){
     problem[id].id=id+1;
@@ -10,10 +12,46 @@ void createproblem(){
     id++;
 }
 
-void viewproblem(){
-    for(int x=0;x>=id;x++){
-        printf("%d:",problem[id].id);
-        printf("%s",problem[id].question);
-        printf("\n%s\n\n",problem[id].answer);
+
+void practice(int max){
+    int count=0;
+    int right=0;
+    for(int i=0;i<max;i++){
+        printf("%s:",problem[i].question);
+        scanf("%s",&answer);
+        if(strcmp(answer,problem[i].answer)){
+            printf("正解!\n");
+            right++;
+        }else{
+            printf("不正解\n");
+        }
+        count++;
+    }
+    printf("正解数 %d/%d",right,answer);
+}
+
+void test(int max){
+    int r;
+    int count=0;
+    int right=0;
+    srand((unsigned)time(NULL));
+    for(int i;i<5;i++){
+        r=rand()%max;
+        printf("%s:",problem[r].question);
+        scanf("%s",&answer);
+        if(strcmp(answer,problem[r].answer)){
+            printf("正解!\n");
+            right++;
+        }else{
+            printf("不正解\n");
+        }
+        count++;
+    }
+    printf("正解数 %d/%d",right,answer);
+}
+
+void problemlist(int max){
+    for(int i=0;i<max;i++){
+        printf("%s\n",problem[i].question);
     }
 }
--- ui.c
+++ ui.c
@@ -1,1 +1,75 @@
-test
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "ui.h"
+
+int ui(void)
+
+{
+    int pos = 0;
+    char key;
+
+    while(1)
+    {
+        system("cls");
+
+        printf("\n\n");
+
+        if(pos == 0)
+            printf("\x1b[33m[問題作成]\x1b[39m");
+        else
+            printf("[問題作成]");
+
+        if(pos == 1)
+            printf("\t\x1b[33m[練習]\x1b[39m\n\n");
+        else
+            printf("\t[練習]\n\n");
+
+        if(pos == 2)
+            printf("\x1b[33m[テスト]\x1b[39m");
+        else
+            printf("[テスト]");
+
+        if(pos == 3)
+            printf("\t\x1b[33m[問題一覧]\x1b[39m\n");
+        else
+            printf("\t[問題一覧]\n");
+        if(pos == 4)
+            printf("\n\x1b[33m[終了]\x1b[39m\n");
+        else
+            printf("\n[終了]\n");
+
+        key = getch();
+
+        if(key == 'w' || key == 'W')
+        {
+            if(pos == 2) pos = 0;
+            else if(pos == 3) pos = 1;
+            else if(pos == 4) pos = 2;   // 4から上
+        }
+
+        if(key == 's' || key == 'S')
+        {
+            if(pos == 0) pos = 2;
+            else if(pos == 1) pos = 3;
+            else if(pos == 2 || pos == 3) pos = 4;  // 一番下へ
+        }
+
+        if(key == 'a' || key == 'A')
+        {
+            if(pos == 1) pos = 0;
+            else if(pos == 3) pos = 2;
+        }
+
+        if(key == 'd' || key == 'D')
+        {
+            if(pos == 0) pos = 1;
+            else if(pos == 2) pos = 3;
+        }
+
+        if(key == 13)
+        {
+            return pos;
+        }
+    }
+}
