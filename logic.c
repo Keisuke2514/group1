3c30e7e2a049fa43f6bd254d157ec5186dde410f
Keisuke2514 <s2514@oita.kosen-ac.jp>
2026-06-19 15:54:33 +0900

    fix-logic2

--- logic.c
+++ logic.c
@@ -1,5 +1,7 @@
 #include "logic.h"
 #include <stdio.h>
+#include<string.h>
+#include<timr.h>
 
 void createproblem(){
     problem[id].id=id+1;
@@ -8,4 +10,47 @@ void createproblem(){
     printf("答えを入力:");
     scanf("%s",&problem[id].answer);
     id++;
+}
+
+
+void practice(int max){
+    int count=0;
+    int right=0;
+    for(int i=0;i<max;i++){
+        printf("%s:",problem[i].question);
+        scanf("%s",&answer);
+        if(strcmp(answer,problem[i].answer)){
+            printf("正解!\n");
+            right++;
+        }else{
+            printf("不正解\n");
+        }
+        count++;
+    }
+    printf("正解数 %d/%d",right,answer);
+}
+
+void test(int max){
+    int r;
+    int count=0;
+    srand((unsigned)time(NULL));
+    for(int i;i<5;i++){
+        r=rand()%max;
+        printf("%s:",problem[r].question);
+        scanf("%s",&answer);
+        if(strcmp(answer,problem[r].answer)){
+            printf("正解!\n");
+            right++;
+        }else{
+            printf("不正解\n");
+        }
+        count++;
+    }
+    printf("正解数 %d/%d",right,answer);
+}
+
+void problemlist(int max){
+    for(int i=0;i<max;i++){
+        printf("%s:",problem[r].question\n);
+    }
 }
--- logic.h
+++ logic.h
@@ -7,5 +7,6 @@ extern int id=0;
     char answer[1024];
  }Problem;
  Problem problem[1024];
- 
+ char answer[1024];
+
  #endif
